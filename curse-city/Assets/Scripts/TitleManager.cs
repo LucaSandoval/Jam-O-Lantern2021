4ef65fb3cef6fd4c04fd539858f015c3a1e6f4aa
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2021-11-04 23:29:25 -0400

    main theme added

--- curse-city/Assets/Scripts/TitleManager.cs
+++ curse-city/Assets/Scripts/TitleManager.cs
@@ -6,6 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+
+    private SoundManager soundManager;
+
+    public void Start()
+    {
+        soundManager = GetComponent<SoundManager>();
+        soundManager.Play("Title");
+    }
+
+
     public void Play()
     {
         SceneManager.LoadScene(1);
